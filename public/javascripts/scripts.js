95dacfa7a90213816e652f1346a48a5e647d18ac
muzammil-85 <muzukondeth85@gamil.com>
2022-01-28 22:13:15 +0530

    cart

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -11,3 +11,26 @@ function addToCart(proId){
       }
     })
   }
+
+function ChangeQuantity(cartId,proId,count){
+  let quantity = parseInt(document.getElementById(proId).innerHTML)
+  count = parseInt(count)
+  $.ajax({
+      url:'/change-product-quantity',
+      data:{
+          cart:cartId,
+          product:proId,
+          count:count,
+          quantity:quantity
+      },
+      method:'post',
+      success:(response)=>{
+          if(response.removeProduct){
+          alert("Product Removed from Cart")
+          location.reload()
+          }else{
+              document.getElementById(proId).innerHTML = quantity + count
+          }
+      }
+  })
+}
